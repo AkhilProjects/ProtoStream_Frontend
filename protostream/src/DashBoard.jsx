1134989c6a66e73f0f7adf35b0dd382d8fd34137
akhilsiraswal <akhilsiraswal@gmail.com>
2021-01-11 10:20:29 +0530

    some minor changes

--- protostream/src/DashBoard.jsx
+++ protostream/src/DashBoard.jsx
@@ -10,45 +10,58 @@ import {Redirect} from "react-router-dom";
 
 const Dashboard = () => {
   const {user} = isAuthenticated();
-  const {Profilename,ProfileID,ProfileBranch,ProfileYear} = user.profiledata;
+  const {Profilename,ProfileBranch,ProfileYear} = user.profiledata;
+
+  const onClickLogout = () =>{
+    signout(() =>{
+      return <Redirect to="/login"/>
+    })
+  }
+
+
+  const dashboardForm = () =>{
+    return (
+      <div className="dashboard">
+        <div className="Container">
+          <div className="side-bar">
+            <img className="student-img" src={profile} alt="student-imh" />
+            <h4 className="name">{Profilename}</h4>
+            <p className="student"> {ProfileBranch}</p>
+            <p className="student">{ProfileYear}</p>
+  
+            <button className="log-out" onClick={onClickLogout} >
+              <i className="fas fa-sign-out-alt"></i> Log Out
+            </button>
+          </div>
+  
+          <div className="content">
+            <Box heading="My Projects" para="Your projects and startups" />
+            <Box
+              heading="Resource Box"
+              para="Your curated list of tools and business courses to help you along
+              the journey"
+            />
+            <Box
+              heading="Learn Entrepreneurship"
+              para="Enroll into Entrepreneurship courses at MIT OCW"
+            />
+            <div className="reg">
+                <h2>Register a Startup</h2>
+                <button>+</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 
   
   return (
-    <div className="dashboard">
-      <div className="Container">
-        <div className="side-bar">
-          <img className="student-img" src={profile} alt="student-imh" />
-          <h4 className="name">{Profilename}</h4>
-          <p className="student"> {ProfileBranch}</p>
-          <p className="student">{ProfileYear}</p>
+    <div>
 
-          <button className="log-out" onClick={() =>{
-            signout(() =>{
-              return <Redirect to="/"/>
-            })
-          }} >
-            <i className="fas fa-sign-out-alt"></i> Log Out
-          </button>
-        </div>
-
-        <div className="content">
-          <Box heading="My Projects" para="Your projects and startups" />
-          <Box
-            heading="Resource Box"
-            para="Your curated list of tools and business courses to help you along
-            the journey"
-          />
-          <Box
-            heading="Learn Entrepreneurship"
-            para="Enroll into Entrepreneurship courses at MIT OCW"
-          />
-          <div className="reg">
-              <h2>Register a Startup</h2>
-              <button>+</button>
-          </div>
-        </div>
-      </div>
+      {dashboardForm()}
+      {/* {onClickLogout()} */}
     </div>
   );
 };
