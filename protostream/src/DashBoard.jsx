36f00b3d7a38560d7058c5cc19aa6aab5d978717
Himanshu <hp200229@gmail.com>
2021-05-03 11:19:33 +0530

    resources completed

--- protostream/src/DashBoard.jsx
+++ protostream/src/DashBoard.jsx
@@ -3,7 +3,7 @@ import "./css/DashBoard/dashboard.css";
 import profile from "./assets/images/profile.svg";
 import Box from "./components/Dashboard/Box";
 import { isAuthenticated, signout } from "./auth";
-import {useHistory} from "react-router-dom";
+import {NavLink, useHistory} from "react-router-dom";
 
 
 const Dashboard = () => {
@@ -30,26 +30,33 @@ const Dashboard = () => {
             <h4 className="name">{Profilename}</h4>
             <p className="student"> {ProfileBranch}</p>
             <p className="student">{ProfileYear}</p>
-  
-            <button className="log-out" onClick={onClickLogout} >
+
+            <button className="log-out" onClick={onClickLogout}>
               <i className="fas fa-sign-out-alt"></i> Log Out
             </button>
           </div>
-  
+
           <div className="content">
-            <Box heading="My Projects" para="Your projects and startups" onClick={onClickMyProjects}/>
             <Box
-              heading="Resource Box"
-              para="Your curated list of tools and business courses to help you along
-              the journey"
+              heading="My Projects"
+              para="Your projects and startups"
+              onClick={onClickMyProjects}
             />
+            {/* <NavLink to="/resources" className="navlink"> */}
+              <Box
+                heading="Resource Box"
+                para="Your curated list of tools and business courses to help you along
+              the journey"
+              />
+            {/* </NavLink> */}
+
             <Box
               heading="Learn Entrepreneurship"
               para="Enroll into Entrepreneurship courses at MIT OCW"
             />
             <div className="reg">
-                <h2>Register a Startup</h2>
-                <button>+</button>
+              <h2>Register a Startup</h2>
+              <button>+</button>
             </div>
           </div>
         </div>
