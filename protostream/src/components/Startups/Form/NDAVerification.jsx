2af73f44f87d9f5db9b6c68c32d462a6aa0fc8ad
Himanshu Pandey <hp200229@gmail.com>
2021-06-19 17:40:18 +0530

    nda

--- protostream/src/components/Startups/Form/NDAVerification.jsx
+++ protostream/src/components/Startups/Form/NDAVerification.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import "../css/Ndaverify.css";
 
 function NDAVerification() {
     return (
-        <div>
-            NDA Verify
+      <div className="Nda-verify">
+        <h1 className="heading">NDA Verification</h1>
+        <div className="container">
+          <div className="sub-head">Non Disclosure Agreement </div>
+          <div className="startup-container">
+              <div className="head">Your Startups</div>
+              <div className="all-startups">
+
+              </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default NDAVerification
